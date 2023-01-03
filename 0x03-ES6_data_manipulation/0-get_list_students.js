c5b4b5a222da81057083b52777e833749bd5d051
PROFAY <Oyewaletimmy01@gmail.com>
2023-01-03 15:36:59 +0000

    get list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+  const array = [];
+  array['0'] = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  array['1'] = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  array['2'] = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+  return array;
+}
