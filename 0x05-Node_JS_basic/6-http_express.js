f6d98c924dcf13ab53ef4bd8bee84653b832664f
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-21 12:28:45 +0000

    express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const app = express();
-const port = 1245
+const port = 1245;
 const hostname = 'localhost';
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!')
-})
+  res.send('Hello Holberton School!');
+});
 
-module.exports = app.listen(port, hostname)
+module.exports = app.listen(port, hostname);
