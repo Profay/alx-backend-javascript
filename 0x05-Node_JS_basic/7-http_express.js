bc1e8c149237e4d0f39c1c196db973ffd0429a43
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-21 12:40:21 +0000

    express read

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const path = process.argv[2];
+const port = 1245;
+
+const countStudents = (path) => {
+  const promise = (res, rej) => {
+    fs.readFile(path, 'utf8', (err, resData) => {
+      if (!err) {
+        const printOut = [];
+        let printItem; // item to printed
+        const data = resData.toString().split('\n');
+        let students = data.filter((item) => item);
+        students = students.map((item) => item.split(','));
+        printItem = `Number of students: ${students.length - 1}`;
+        console.log(printItem);
+        printOut.push(printItem);
+
+        const fields = {};
+        for (const student in students) {
+          if (student !== 0) {
+            if (!fields[students[student][3]]) {
+              fields[students[student][3]] = [];
+            }
+            fields[students[student][3]].push(students[student][0]);
+          }
+        }
+        delete fields.field;
+        for (const key of Object.keys(fields)) {
+          printItem = `Number of students in ${key}: ${
+            fields[key].length}. List: ${fields[key].join(', ')}`;
+          console.log(printItem);
+          printOut.push(printItem);
+        }
+        res(printOut);
+      } else {
+        rej(new Error('Cannot load the database'));
+      }
+    });
+  };
+
+  return new Promise(promise);
+};
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+  })
+
+app.get('/students', (req, res) => {
+    countStudents(path)
+      .then((data) => {
+        res.send(`This is the list of our students\n${data.join('\n')}`);
+      })
+      .catch((error) => {
+        res.end(error);
+      });
+  }
+);
+
+
+module.exports = app.listen(port, 'localhost');
