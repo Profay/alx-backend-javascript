de598877daeb57e77d6b92a49e8cc0e393a851a0
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-24 17:30:59 +0000

    update condition

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -14,4 +14,7 @@ describe ("calculateNumber", () => {
     it("should be 1 + 1.1 = 2", () => {
         assert.equal(calculateNumber(1, 1.1), 2);
     });
+    it("should be 1.5 + 1.5 = 4", () => {
+        assert.equal(calculateNumber(1.5, 1.5), 4);
+    });
 });
