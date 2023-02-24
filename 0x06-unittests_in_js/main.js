ec1d36e66c7e89a3657bb2973b1cf5c39162d16f
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-24 17:47:36 +0000

    using type

--- 0x06-unittests_in_js/main.js
+++ 0x06-unittests_in_js/main.js
@@ -1,2 +1,2 @@
-const calculateNumber = require("./0-calcul.js");
-console.log(calculateNumber(1, 8.5))
+const calculateNumber = require("./1-calcul.js");
+console.log(calculateNumber('DIVIDE', 1, 3))
