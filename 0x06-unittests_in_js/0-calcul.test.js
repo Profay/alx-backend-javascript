a4f88b03aee35ea5fd0e28e8afdfd252432dfcb9
PROFAY <Oyewaletimmy01@gmail.com>
2023-02-24 17:28:03 +0000

    unnitest

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const calculateNumber = require("./0-calcul.js");
+const assert = require("assert");
+
+describe ("calculateNumber", () => {
+    it("should be 1 + 1 = 2", () => {
+        assert.equal(calculateNumber(1, 1), 2);
+    });
+    it("should be 1 + 2 = 3", () => {
+        assert.equal(calculateNumber(1, 2), 3);
+    });
+    it("should be 1 + 3 = 4", () => {
+        assert.equal(calculateNumber(1, 3), 4);
+    });
+    it("should be 1 + 1.1 = 2", () => {
+        assert.equal(calculateNumber(1, 1.1), 2);
+    });
+});
